6a1c422256ef717e31b60d0c27cedde73ccd4947
Facundo Gonzalez <facundo.siquot@gmail.com>
2015-05-23 15:49:50 -0300

    add some information and loging

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# node-js-getting-started
+# node phonegap app builder
 
 A barebones Node.js app using [Express 4](http://expressjs.com/).
 
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var debug = require('debug')('phonegapbuilder:index');
+
+debug('builder has started');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -8,6 +12,11 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/builderauthcallback', function(request, response) {
+    debug(request);
+  response.send('success!');
+});
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  debug('listening on port', app.get('port'));
 });
